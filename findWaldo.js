a80ecab5337349779f40c2ca734fa63f5853e46a
Sam Schantz <samvschantz@gmail.com>
2018-03-15 17:40:17 +0000

    Found Waldo with index

--- findWaldo.js
+++ findWaldo.js
@@ -1,9 +1,24 @@
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
+// function actionWhenFound(i) {
+//   console.log("Found Waldo at " + i +"!");
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(element, i){
+    if (element === "Waldo"){
+      found(i);
     }
-  }
+  })
 }
 
 function actionWhenFound(i) {
@@ -11,24 +26,3 @@ function actionWhenFound(i) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-
-
-
-
-
-
-
-// function findWaldo(arr, found) {
-//   arr.forEach(function(element){
-//     if (element === "Waldo"){
-//       found();
-//     }
-//   })
-// }
-
-// function actionWhenFound() {
-//   console.log("Found him!");
-// }
-
-// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);

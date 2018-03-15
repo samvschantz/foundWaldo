4495a970868dbbe98fcd5344143b7481022b79cb
Sam Schantz <samvschantz@gmail.com>
2018-03-15 17:33:12 +0000

    Found waldo @ index

--- findWaldo.js
+++ findWaldo.js
@@ -1,13 +1,34 @@
 function findWaldo(arr, found) {
-  arr.forEach(function(element){
-    if (element === "Waldo"){
-      found();
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
     }
-  })
+  }
 }
 
-function actionWhenFound() {
-  console.log("Found him!");
+function actionWhenFound(i) {
+  console.log("Found Waldo at " + i +"!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+
+
+
+
+
+// function findWaldo(arr, found) {
+//   arr.forEach(function(element){
+//     if (element === "Waldo"){
+//       found();
+//     }
+//   })
+// }
+
+// function actionWhenFound() {
+//   console.log("Found him!");
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
